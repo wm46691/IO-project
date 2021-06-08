fe6e0029ffa3f6ee2e60ea8a0788a1e5b5f8387a
Your Name <igor@localhost>
2021-06-08 20:21:19 +0200

    simpleks zostal

--- generowanie_zadan.py
+++ generowanie_zadan.py
@@ -4,34 +4,56 @@ import rozniczkowanie
 import matplotlib.pyplot as plt
 import nieliniowe
 import calkowanie
+from os import system
 #import simplex
+
 def uklady_liniowe():
-    print('Znając macierz współczynników A oraz wektor rozwiązania b. Wyznacz macierze rozkładu L i U stosując metodę Doolittle\'a i oblicz rozwiązanie.')
-    
     A = np.random.randint(1, 99, (3,3))
     b = np.random.randint(1, 99, (3, 1))
-    print('Macierz A: ')
-    for i, v in enumerate(A):
-        for j, vv in enumerate(v):
-            print(f'{vv}*x{j+1}', end = ' ')
-        print('\n')
-
-    print(f'Wektor wyniku b:\n {b}\n')
-
+    
     L, U = LU.Doolittle(A)
     X, Y = LU.Rozwiazanie(L, U, A, b)
-    przelacznik = True
-    while przelacznik:
-        schowek = input('Naciśnij ''k'', aby zobaczyć kroki lub ''w'', aby zobaczyć wynik.')
-        if schowek == 'k':
-            print(f'Macierz rozkładu L: \n {L}\n')
-            print(f'Macierz rozkładu U: \n {U}\n')
-            print(f'Rozwiązanie układu inv(L)*b = z: \n {Y}\n')
-        elif schowek == 'w':
-            print(X)
-            przelacznik = False
+    while True:
+        print('Znając macierz współczynników A oraz wektor rozwiązania b. Wyznacz macierze rozkładu L i U stosując metodę Doolittle\'a i oblicz rozwiązanie.')
 
-    return X
+        print('Macierz A: ')
+        for i, v in enumerate(A):
+            for j, vv in enumerate(v):
+                print(f'{vv}*x{j+1}', end = ' ')
+            print('\n')
+
+        print(f'Wektor wyniku b:\n {b}\n')
+
+        input('Naciśnij ENTER, aby kontynuować.')
+
+        while True:
+            schowek = input('Naciśnij k, aby zobaczyć kroki lub d, aby przejść do etapu sprawdzenia wyniku.')
+            if schowek == 'k' or schowek == 'd':
+                pass
+            else:
+                print('Zły klawisz! Wybierz przycisk k lub d.')
+                input('Naciśnij ENTER, aby kontynuować')
+
+        
+            if schowek == 'k':
+                print(f'Macierz rozkładu L: \n {L}\n')
+                print(f'Macierz rozkładu U: \n {U}\n')
+                print(f'Rozwiązanie układu inv(L)*b = z: \n {Y}\n')
+            elif schowek == 'd':
+                print('Wpisz swój wynik')
+                wynik = np.zeros((3, 1))
+                for i in range(3):
+                    wynik[i] = input()
+
+                if np.all(wynik == X):
+                    print('Prawidłowy wynik!')
+                    return 0
+
+                else:
+                    print('Podałeś zły wynik. Oblicz jeszcze raz')
+                    input('Naciśnij ENTER, aby kontynuować.')
+                    break
+
 
 def zadania_rozniczkowanie():
     funkcje = {'sin':np.sin, 'cos':np.cos}
@@ -41,26 +63,28 @@ def zadania_rozniczkowanie():
     a = np.random.randint(1, 20)
     b = np.random.randint(a*2, a*20)
     ilosc_punktow = 1000
-    print(f'Narysuje pochodna funkcji {klucze[index]}(x) na przedziale od {a} do {b}')
+
     y = rozniczkowanie.pochodne_punktowe(a, b, ilosc_punktow, funkcje[klucze[index]], 0.25, 2)
     x = np.linspace(a, b, ilosc_punktow)
-    
     while True:
-        schowek = input('Nacisnij ''k'', aby zobaczyć przykładowe punkty lub w, aby zobaczyć wynik.')
-        if schowek == 'k':
-            print(y)
-        elif schowek == 'w':
-            break
+        print(f'Narysuje pochodna funkcji {klucze[index]}(x) na przedziale od {a} do {b}')
+        while True:
+            schowek = input('Nacisnij k, aby zobaczyć przykładowe punkty lub d, aby wyświetlić wynik.')
+            if schowek != 'k' and schowek != 'd':
+                print('Zły klawisz! Wybierz przycisk k lub d.')
+                input('Naciśnij ENTER, aby kontynuować')
+            elif schowek == 'k':
+                print(y)
+            elif schowek == 'd':
+                plt.plot(x, y)
+                plt.show()
+                return 0
 
-    plt.plot(x, y)
-    plt.show()
-    
+   
 def uklady_nieliniowe():
-    
     funkcje = {'sin':np.sin, 'cos':np.cos, 'tan':np.tan, }
     klucze = [i for i in funkcje.keys()]
     index = np.random.randint(0, len(klucze))
-    print(f'Znajdź rozwiązanie równania nieliniowego {klucze[index]}(x) stosując metode bisekcji.', end = ' ')   
     while True:
         a = np.random.randint(1, 10)
         b = np.random.randint(a*11, a*20)
@@ -68,15 +92,28 @@ def uklady_nieliniowe():
         x, steps = nieliniowe.bisekcja(f, a, b)
         if x != None:
             break
-    print(f'W przedziale od {a} do {b} z dokladnością 0.0001')
-    
     while True:
-        schowek = input('Nacisnij ''k'', aby zobaczyć kroki lub w, aby zobaczyć wynik.')
-        if schowek == 'k':
-            print(steps)
-        elif schowek == 'w':
-            print(x)
-            break
+        print(f'Znajdź rozwiązanie równania nieliniowego {klucze[index]}(x) stosując metode bisekcji.', end = ' ') 
+        print(f'W przedziale od {a} do {b} z dokladnością 0.0001')
+    
+        while True:
+            schowek = input('Nacisnij k, aby zobaczyć kroki lub d, aby sprawdzić wynik.')
+            if schowek != 'k' and schowek != 'd':
+                print('Zły klawisz! Wybierz przycisk k lub d.')
+                input('Naciśnij ENTER, aby kontynuować')
+            elif schowek == 'k':
+                print(steps)
+            elif schowek == 'd':
+                print(x)
+                wynik = float(input('Podaj uzyskany wynik'))
+                if wynik == steps[-1]:
+                    print('Prawidłowy wynik!')
+                    return 0
+                else:
+                    print('Zły wynik! Spróbuj jeszcze raz.')
+                    input('Naciśnij ENTER, aby kontynuować')
+                    break
+
 
 
 def zadania_calkowanie():
@@ -85,19 +122,32 @@ def zadania_calkowanie():
     index = np.random.randint(0, len(klucze))
     a = np.random.randint(1, 20)
     b = np.random.randint(a*2, a*20)
-
-    print(f'Oblicz pole pod wykresem funkcji {klucze[index]}(x) w przedziale od {a} do {b} za pomocą metody prostokątów o szerokości prostokąta {(b-a)/5}')
     wynik, szerokosc, pole_kroki = calkowanie.metoda_prostokatow(funkcje[klucze[index]], a, b)
 
-    while True:
-        schowek = input('Naciśnij ''k'', aby zobaczyć kroki lub ''w'', aby zobaczyć wynik.')
-        if schowek == 'k':
-            print(szerokosc)
-            print(pole_kroki)
-        elif schowek == 'w':
-            print(wynik)
-            break
 
+
+    while True:
+        print(f'Oblicz pole pod wykresem funkcji {klucze[index]}(x) w przedziale od {a} do {b} za pomocą metody prostokątów o szerokości prostokąta {(b-a)/5}')
+    
+
+        while True:
+            schowek = input('Naciśnij k, aby zobaczyć kroki lub d, aby sprawdzić wynik.')
+            if schowek != 'k' and schowek != 'd':
+                print('Zły klawisz! Wybierz przycisk k lub d.')
+                input('Naciśnij ENTER, aby kontynuować')
+            elif schowek == 'k':
+                print('Pola poszczególnych prostokątów.')
+                print(np.abs(pole_kroki))
+            elif schowek == 'd':
+                print(wynik)
+                wynik_użytkownika = float(input('Podaj wynik: '))
+                if wynik_użytkownika == wynik:
+                    print('Prawidłowy wynik!')
+                    return 0
+                else:
+                    print('Zły wynik! Spróbuj jeszcze raz.')
+                    input('Naciśnij ENTER, aby kontynuować')
+                    break
 
 
 # def zadania_simplex():
@@ -108,4 +158,6 @@ def zadania_calkowanie():
 #     return simplex.simplex(A, B)
 
 
-zadania_rozniczkowanie()
+
+
+zadania_calkowanie()
